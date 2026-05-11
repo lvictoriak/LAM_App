7de97fa70ca6bf82c0f8e1b19d33df30fb7d5c13
lvictoriak <vika7667@list.ru>
2026-05-12 03:30:44 +0500

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LAM_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void menu_button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void payment_btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void price_btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void timetable_btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void trial_btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void attendance_btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void note_btn1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
